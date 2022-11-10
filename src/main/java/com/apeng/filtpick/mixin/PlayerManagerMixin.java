24820a9e5c8a7aa98b4c5e005122dcd6cd1e3001
APeng <976310604@qq.com>
2022-11-10 15:45:11 +0800

    Fix PlayerMixins

--- src/main/java/com/apeng/filtpick/mixin/PlayerManagerMixin.java
+++ src/main/java/com/apeng/filtpick/mixin/PlayerManagerMixin.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.network.ServerPlayerInteractionManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
@@ -20,7 +21,7 @@ import java.util.Optional;
 @Mixin(PlayerManager.class)
 public abstract class PlayerManagerMixin {
     @Inject(method = "respawnPlayer", at = @At("TAIL"),locals = LocalCapture.CAPTURE_FAILEXCEPTION)
-    private void synchronizeListModeWithClient(ServerPlayerEntity player, boolean alive, CallbackInfoReturnable<ServerPlayerEntity> cir, BlockPos blockPos, float f, boolean bl, ServerWorld serverWorld, Optional optional, ServerWorld serverWorld2, ServerPlayerEntity serverPlayerEntity){
+    private void synchronizeListModeWithClient(ServerPlayerEntity player, boolean alive, CallbackInfoReturnable<ServerPlayerEntity> cir, BlockPos blockPos, float f, boolean bl, ServerWorld serverWorld, Optional optional, ServerPlayerInteractionManager serverPlayerInteractionManager, ServerWorld serverWorld2, ServerPlayerEntity serverPlayerEntity){
         PacketByteBuf modeInfo = new PacketByteBuf(PacketByteBufs.create().writeBoolean(((ServerPlayerEntityDuck)player).getFiltPickIsWhiteListMode()));
         ServerPlayNetworking.send(player,new Identifier("syn_listmode"), modeInfo);
     }
