d1099f5d780225c91582154eb155401fdba21a06
APeng <976310604@qq.com>
2022-11-18 21:17:37 +0800

    Complete the tool-tip of destruction-button.

--- src/main/java/com/apeng/filtpick/guis/custom/FiltPickScreen.java
+++ src/main/java/com/apeng/filtpick/guis/custom/FiltPickScreen.java
@@ -12,14 +12,22 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FiltPickScreen extends HandledScreen<FiltPickScreenHandler> {
     static TexturedButtonWidget whiteModeButton,blackModeButton;
     static TexturedButtonWidget destructionModeOnButton,destructionModeOffButton;
     public static boolean filtPickIsWhiteListMode = false;
     public static boolean filtPickIsDestructionMode = false;
+    private final List<Text> tooltipOfWhiteMode = new ArrayList<>();
+    private final List<Text> tooltipOfBlackMode = new ArrayList<>();
+    private final List<Text> tooltipOfDestructionOn = new ArrayList<>();
+    private final List<Text> tooltipOfDestructionOff = new ArrayList<>();
     private static final Identifier FILTPICK_RETURN_BUTTON_TEXTURE = new Identifier("filtpick","gui/filtpick_return_button.png");
     //A path to the gui texture. In this example we use the texture from the dispenser
     private static final Identifier FILTPICK_SCREEN_TEXTURE = new Identifier("filtpick", "gui/filtpick_screen.png");
@@ -70,13 +78,13 @@ public class FiltPickScreen extends HandledScreen<FiltPickScreenHandler> {
             sendC2SPacketToSetDestructionMode(false);
             this.remove(destructionModeOnButton);
             this.addDrawableChild(destructionModeOffButton);
-        }, (button, matrices, mouseX, mouseY) -> FiltPickScreen.this.renderTooltip(matrices,Text.translatable("destruction_mode_on_explanation"),destructionModeOnButton.x-100, destructionModeOnButton.y),Text.of("destruction_mode_on_explanation"));
+        }, (button, matrices, mouseX, mouseY) -> FiltPickScreen.this.renderTooltip(matrices, tooltipOfDestructionOn, destructionModeOnButton.x, destructionModeOnButton.y),Text.of("destruction_mode_on_explanation"));
         destructionModeOffButton = new TexturedButtonWidget(this.x + 10 + 2 + 12, this.y + 4, 12, 11, 0, 0, 12, DESTRUCTION_OFF_BUTTON_TEXTURE,256,256,button -> {
             filtPickIsDestructionMode = !filtPickIsDestructionMode;//Switch
             sendC2SPacketToSetDestructionMode(true);
             this.remove(destructionModeOffButton);
             this.addDrawableChild(destructionModeOnButton);
-        }, (button, matrices, mouseX, mouseY) -> FiltPickScreen.this.renderTooltip(matrices,Text.translatable("destruction_mode_off_explanation"),destructionModeOffButton.x-100, destructionModeOffButton.y),Text.of("destruction_mode_off_explanation"));
+        }, (button, matrices, mouseX, mouseY) -> FiltPickScreen.this.renderTooltip(matrices, tooltipOfDestructionOff, destructionModeOffButton.x, destructionModeOffButton.y),Text.of("destruction_mode_off_explanation"));
         if(filtPickIsDestructionMode){
             this.addDrawableChild(destructionModeOnButton);
         }
@@ -85,18 +93,20 @@ public class FiltPickScreen extends HandledScreen<FiltPickScreenHandler> {
         }
     }
     private void initModeButton() {
+        initTooltipsOfModeButton();
+        initTooltipsOfDestructionButton();
         whiteModeButton = new TexturedButtonWidget(this.x + 10, this.y + 4, 12, 11, 0, 0, 12, WHITELIST_BUTTON_TEXTURE, 256, 256, button -> {
             filtPickIsWhiteListMode = !filtPickIsWhiteListMode;//Switch
             sendC2SPacketToSetWhiteMode(false);
             this.remove(whiteModeButton);
             this.addDrawableChild(blackModeButton);
-        }, (button, matrices, mouseX, mouseY) -> FiltPickScreen.this.renderTooltip(matrices,Text.translatable("whitelist_mode_explanation"),whiteModeButton.x-100, whiteModeButton.y),Text.of("whitelist_mode_explanation"));
+        }, (button, matrices, mouseX, mouseY) -> FiltPickScreen.this.renderTooltip(matrices,tooltipOfWhiteMode,whiteModeButton.x, whiteModeButton.y),Text.of("whitelist_mode_explanation"));
         blackModeButton = new TexturedButtonWidget(this.x + 10, this.y + 4, 12, 11, 0, 0, 12, BLACKLIST_BUTTON_TEXTURE,256,256,button -> {
             filtPickIsWhiteListMode = !filtPickIsWhiteListMode;//Switch
             sendC2SPacketToSetWhiteMode(true);
             this.remove(blackModeButton);
             this.addDrawableChild(whiteModeButton);
-        }, (button, matrices, mouseX, mouseY) -> FiltPickScreen.this.renderTooltip(matrices,Text.translatable("blacklist_mode_explanation"),blackModeButton.x-100, blackModeButton.y),Text.of("blacklist_mode_explanation"));
+        }, (button, matrices, mouseX, mouseY) -> FiltPickScreen.this.renderTooltip(matrices,tooltipOfBlackMode,blackModeButton.x, blackModeButton.y),Text.of("blacklist_mode_explanation"));
         if(filtPickIsWhiteListMode){
             this.addDrawableChild(whiteModeButton);
         }
@@ -104,7 +114,17 @@ public class FiltPickScreen extends HandledScreen<FiltPickScreenHandler> {
             this.addDrawableChild(blackModeButton);
         }
     }
-
+    private void initTooltipsOfModeButton(){
+        tooltipOfWhiteMode.add(Text.translatable("whitelist_mode").formatted(Formatting.GREEN));
+        tooltipOfWhiteMode.add(Text.translatable("whitelist_mode_explanation").formatted(Formatting.DARK_GRAY,Formatting.ITALIC));
+        tooltipOfBlackMode.add(Text.translatable("blacklist_mode").formatted(Formatting.RED));
+        tooltipOfBlackMode.add(Text.translatable("blacklist_mode_explanation").formatted(Formatting.DARK_GRAY,Formatting.ITALIC));
+    }
+    private void initTooltipsOfDestructionButton(){
+        tooltipOfDestructionOff.add(Text.translatable("destruction_mode_off").formatted(Formatting.GRAY));
+        tooltipOfDestructionOn.add(Text.translatable("destruction_mode_on").formatted(Formatting.RED));
+        tooltipOfDestructionOn.add(Text.translatable("destruction_mode_on_explanation").formatted(Formatting.DARK_GRAY,Formatting.ITALIC));
+    }
     @NotNull
     private TexturedButtonWidget createReturnButton() {
         return new TexturedButtonWidget(this.x + 154, this.y + 4, 12, 11, 0, 0, 12, FILTPICK_RETURN_BUTTON_TEXTURE, button
